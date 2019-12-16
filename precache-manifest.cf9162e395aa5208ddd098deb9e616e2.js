4b71d52869330f9773e6390e9eeb6a8615ccbf16
Chandu J S <app.coder@outlook.in>
2019-12-16 15:02:39 +0400

    Updates

---
+++ precache-manifest.cf9162e395aa5208ddd098deb9e616e2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081c083ded17dbffef98d28b7eb58ce0",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "d1f390104fa6341b9c19",
+    "url": "/react/static/js/2.9465672c.chunk.js"
+  },
+  {
+    "revision": "c61132261c13f89f4e38",
+    "url": "/react/static/js/main.907464ee.chunk.js"
+  },
+  {
+    "revision": "82d9877287711573aade",
+    "url": "/react/static/js/runtime-main.bf384fd5.js"
+  }
+]);
